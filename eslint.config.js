072aeb6d5376c55abf48e5330f540ab4881f1a94
Alexander Koval <alexander.v.koval@gmail.com>
2024-06-15 18:34:14 -0400

    Add configuration files for docker, editor, and linter

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+import globals from 'globals';
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+
+export default [
+  {
+    ...js.configs.recommended,
+    files: ['**/*.js'],
+    plugins: { prettier: eslintPluginPrettier },
+    rules: {
+      ...eslintPluginPrettier.configs.recommended.rules,
+      ...eslintConfigPrettier.rules,
+      quotes: ['error', 'single', { avoidEscape: true, allowTemplateLiterals: false }],
+      'class-methods-use-this': 'off',
+      'comma-dangle': 'off',
+      'object-curly-newline': 'off',
+      'operator-linebreak': 'off',
+      'implicit-arrow-linebreak': 'off',
+      'function-paren-newline': 'off',
+      'import/no-extraneous-dependencies': 'off',
+      'import/extensions': 'off',
+      'import/no-absolute-path': 'off',
+      'generator-star-spacing': 'off',
+      'no-prototype-builtins': 'off',
+      'no-underscore-dangle': 'off',
+      'no-plusplus': 'off',
+      'no-undef': 'warn',
+      'no-case-declarations': 'warn',
+    },
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        myCustomGlobal: 'readonly',
+      },
+    },
+  },
+];
