2e2a59aa6f7c7523560b38cfc8a01e6105a1f3f6
Alexander Koval <alexander.v.koval@gmail.com>
2024-07-02 22:00:14 -0400

    Add more robust handling to parseValue helper function

--- handlers/utils.js
+++ handlers/utils.js
@@ -3,20 +3,31 @@ export const convertToBoolean = (value) => {
   if (typeof value === 'string') {
     const lowerCaseValue = value.trim().toLowerCase();
     return ['yes', 'true', '1'].includes(lowerCaseValue)
-      ? true 
-      : ['no', 'false', '0'].includes(lowerCaseValue) 
-        ? false 
+      ? true
+      : ['no', 'false', '0'].includes(lowerCaseValue)
+        ? false
         : Boolean(value);
   }
   return Boolean(value);
 };
 
-// Handles blank values in import data
-export const parseValue = (value, parser, offset = 0) => {
-  if (
-    value === undefined 
-    || value === null 
-    || value.trim() === ''
-  ) return null;
-  return parser(value) + offset;
+/*
+  Handles blank values in import data. 
+  Offset = 1 as default because many values in csv start at value 0, but we want database table IDs to start at 1.
+*/
+
+export const parseValue = (value, parser, { defaultValue = null, offset = 1 } = {}) => {
+  try {
+    if (value === undefined
+      || value === null 
+      || (typeof value === 'string' && value.trim() === ''))
+    return defaultValue;
+
+    const parsedValue = parser(value);
+    return isNaN(parsedValue)
+      ? defaultValue
+      : parsedValue + offset;
+  } catch (error) {
+    return defaultValue;
+  }
 };
