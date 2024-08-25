29cb6175c0f0f8f7299bacd937d06c1519148bb4
Alexander Koval <alexander.v.koval@gmail.com>
2024-08-25 18:20:44 -0400

    move init_db folder to db directory

--- init_db/11_insert_case_weapons_lookups.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO firearm_proficiency(firearm_proficiency_id, firearm_proficiency)
-VALUES
-  (1, 'No experience'),
-  (2, 'Some experience (some practice before shooting)'),
-  (3, 'More experienced (held permit/license or classes taken)'),
-  (4, 'Very experienced (military, hunter, sharpshooter, years of consistent practice)');
